8be1eb31140bfcd0ec239c6ac6ad97a9ec0085d4
GaganGoswami <gagan.goswami@gmail.com>
2016-10-12 14:45:59 -0400

    Boiler Plate code

---
+++ tools/startMessage.js
@@ -0,0 +1,6 @@
+import colors from 'colors';
+
+// following statement will help lint to ignore console messgae in this file
+/* eslint-disable no-console */
+
+console.log("Starting Build process".green);
